089d34ebdfb7b6e512d9e91c645f5c1c341d4938
rpalloni <rpdev20@gmail.com>
2021-08-31 18:13:39 +0200

    frontend

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -14,8 +14,8 @@
           <div class="column is-4">
             <div class="box has-text-centered">
               <span class="icon is-size-2 has-text-info"><i class="far fa-clock"></i></span>
-              <h2 class="is-size-4 mt-4 mb-4">Learn at your speed</h2>
-              <p>This is just some random placeholder text</p>
+              <h2 class="is-size-4 mt-4 mb-4">Learn at your own speed</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             </div>
           </div>
 
@@ -23,7 +23,7 @@
             <div class="box has-text-centered">
               <span class="icon is-size-2 has-text-info"><i class="far fa-comments"></i></span>
               <h2 class="is-size-4 mt-4 mb-4">Learn with others</h2>
-              <p>This is just some random placeholder text</p>
+              <p>t enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
           </div>
 
@@ -31,7 +31,7 @@
             <div class="box has-text-centered">
               <span class="icon is-size-2 has-text-info"><i class="fas fa-home"></i></span>
               <h2 class="is-size-4 mt-4 mb-4">Learn from anywhere</h2>
-              <p>This is just some random placeholder text</p>
+              <p>uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
             </div>
           </div>
 
